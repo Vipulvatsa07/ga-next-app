e0f51d8a9566ca863878ae9bb8c3209c22c43f0a
Vipulvatsa07 <vatsavipul.8@gmail.com>
2023-01-09 22:58:26 +0530

    ADD

--- pages/experience.jsx
+++ pages/experience.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+// import React from 'react'
 
-const Experience = () => {
-  return (
-    <div>
-        <h1>Assistant Engineer,Ericsson</h1>
-        <h4>•Monitoring of Voice Fixed and Ran Network
-of Telefonica Colombia Location.</h4>
-<h4>
-•Creating managing and Resolving Tickets on
-alarm on various switches NORTEL, EWSD,
-AXE, ALCATEL and NEAX.</h4>
-    </div>
-  )
-}
+// const Experience = () => {
+//   return (
+//     <div>
+//         <h1>Assistant Engineer,Ericsson</h1>
+//         <h4>•Monitoring of Voice Fixed and Ran Network
+// of Telefonica Colombia Location.</h4>
+// <h4>
+// •Creating managing and Resolving Tickets on
+// alarm on various switches NORTEL, EWSD,
+// AXE, ALCATEL and NEAX.</h4>
+//     </div>
+//   )
+// }
 
-export default Experience
+// export default Experience
